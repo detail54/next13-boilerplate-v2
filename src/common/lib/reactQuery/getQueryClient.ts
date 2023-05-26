591bab6d189d2eb03ca7acfb98d6521fa15938a0
daehyun <detail54@nate.com>
2023-05-26 13:12:55 +0900

    react-query > QueryClient 옵션 설정

--- src/common/lib/reactQuery/getQueryClient.ts
+++ src/common/lib/reactQuery/getQueryClient.ts
@@ -1,4 +1,16 @@
 import { QueryClient } from '@tanstack/react-query'
 import { cache } from 'react'
 
-export default cache(() => new QueryClient())
+export default cache(
+  () =>
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: 0,
+          refetchOnMount: false,
+          refetchOnReconnect: false,
+          refetchOnWindowFocus: false,
+        },
+      },
+    }),
+)
